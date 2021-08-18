63c8e69b524db02443fb27372d0942a089bf953e
MilenaGkm <milenagaranin@gmail.com>
2021-08-19 01:06:17 +0300

    added shift selection

--- src/containers/NewRequestShifts/NewRequestShifts.js
+++ src/containers/NewRequestShifts/NewRequestShifts.js
@@ -12,8 +12,16 @@ import Chip from '@material-ui/core/Chip';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
+var eachDayOfInterval = require('date-fns/eachDayOfInterval')
+
 import {
     MuiPickersUtilsProvider,
     KeyboardTimePicker,
@@ -40,22 +48,62 @@ const useStyles = makeStyles((theme) => ({
     textField: {
         marginTop: '18px',
         marginBottom: '15px',
-    }
+    },
+    formControl: {
+        display: 'flex',
+        justifyContent: 'space-around',
+        flexWrap: 'wrap',
+        listStyle: 'none',
+        padding: theme.spacing(0.5),
+        // marginTop: '5px',
+        // marginBottom: '15px',
+    },
+    paper: {
+        marginTop: '5px',
+        marginBottom: '15px',
+        // flex-direction: column
+        // display: "flex",
+        // flexWrap: "wrap",
+        // justifyontent: "space-around",
+    },
+}));
+
+const useStyles2 = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        flexWrap: "wrap",
+        // flex-direction: column
+        justifyontent: "space-around",
+    },
+    cb: {
+        display: "flex",
+        flexWrap: "wrap",
+        // flex-direction: column
+        justifyontent: "space-around",
+    },
 }));
 
 // export default function NewRequestShifts(props) {
 const NewRequestShifts = ({ apiUsers, isLoading, error, fetchUsers, addRequestShift }) => {
 
     const classes = useStyles();
+    const classes2 = useStyles2();
     const [open, setOpen] = useState(false);
-    const [fromDate, setFromDate] = useState(new Date().toLocaleString());
-    const [toDate, setToDate] = useState(new Date().toLocaleString());
+    const [dates, setDates] = useState({ from: new Date(), to: new Date() });
+    const [fromDate, setFromDate] = useState(new Date());
+    const [toDate, setToDate] = useState(new Date());
+    const [generateShiftPeriod, setGenerateShiftPeriod] = useState(false);
+    const [checkbox, setCheckbox] = useState([]);
 
-    // const [msgForm, setMsgForm] = useState({ sender_id: "60e5e3544ea83558e0482710", reciever_ids: [], body: "", subject: "" });
+    function disableWeekends(date) {
+        return date.getDay() === 6;
+        // console.log(date.getDay() === 0 || date.getDay() === 6);
+    }
 
-    // const updateForm = (key, value) => setMsgForm({ ...msgForm, [key]: value })
+    useEffect(() => {
+        fetchUsers()
+    }, [])
 
-    // const handleDateChange = (key, value) => setSelectedDate({...selectedDate, [key]: value})
 
     const handleSubmitReqShift = reqShiftForm => {
         // reqShiftForm.reciever_ids = reqShiftForm.reciever_ids.map(user => user._id)
@@ -63,19 +111,40 @@ const NewRequestShifts = ({ apiUsers, isLoading, error, fetchUsers, addRequestSh
         console.log(reqShiftForm);
     }
 
-    const handleFromDateChange = date => {
-        // props.submitMsg(msgForm)
-        // console.log(key);
-        // console.log(date.toLocaleString());
-        setFromDate(date.toLocaleDateString())
-        // setOpen(false);
-    };
-    const handleToDateChange = date => {
-        // props.submitMsg(msgForm)
-        // console.log(key);
-        setToDate(date)
-        console.log(date.toLocaleDateString());
-        // setOpen(false);
+    useEffect(() => {
+        const numberOfDates = dates.to.getDate() - dates.from.getDate();
+       
+        const checkboxes = []
+        const t = new Date(dates.from)
+        for (let i = 0; i < numberOfDates + 1; i++) {
+            console.log(t.getDay());
+            i > 0 ? t.setDate(t.getDate() + 1) : null;
+            t.getDay() === 6 ? t.setDate(t.getDate() + 1) && i++ : null;
+            if (t.getDay() === 5) {
+                const checkboxesToAdd = {
+                    date: t.toLocaleDateString(),
+                    morningCheckbox: false,
+                    noonCheckbox: false,
+                    eveningCheckbox: "disabled",
+                }
+                checkboxes.push(checkboxesToAdd)
+            } else {
+                const checkboxesToAdd = {
+                    date: t.toLocaleDateString(),
+                    morningCheckbox: false,
+                    noonCheckbox: false,
+                    eveningCheckbox: false,
+                }
+                checkboxes.push(checkboxesToAdd)
+            }
+        }
+        setCheckbox(checkboxes)
+    }, [dates])
+
+    const handleDateChange = (date, name) => {
+        console.log(date.getDay());
+        console.log(name);
+        setDates({ ...dates, [name]: date })
     };
 
     const onSubmit = () => {
@@ -101,7 +170,13 @@ const NewRequestShifts = ({ apiUsers, isLoading, error, fetchUsers, addRequestSh
     const handleClick = () => {
         console.log('You clicked the Chip.');
     };
-    // console.log(selectedDate);
+    // console.log(apiUsers);
+
+    const handleCheckboxChange = (event) => {
+        console.log([event.target.name]);
+        console.log("lol");
+        // setState([ ...checkbox, [event.target.name] = event.target.checked[1] ]);
+    };
 
 
     return (
@@ -111,9 +186,45 @@ const NewRequestShifts = ({ apiUsers, isLoading, error, fetchUsers, addRequestSh
             </Button>
             <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
                 <DialogContent>
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <Grid container justifyContent="space-around">
+                            <KeyboardDatePicker
+                                margin="dense"
+                                id="date-picker-dialog"
+                                label="From"
+                                format="dd/MM/yyyy"
+                                value={dates.from}
+                                // name="fromDate"
+                                // onChange={handleFromDateChange}
+                                // onChange={handleDateChange}
+                                onChange={e => handleDateChange(e, "from")}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change date',
+                                }}
+                                // disabled={disableWeekends}
+                                shouldDisableDate={disableWeekends}
+                            // size="small"
+                            />
+                            <KeyboardDatePicker
+                                // size="small"
+                                margin="dense"
+                                id="date-picker-dialog2"
+                                label="To"
+                                format="dd/MM/yyyy"
+                                value={dates.to}
+                                // name="toDate"
+                                // onChange={handleToDateChange}
+                                onChange={e => handleDateChange(e, "to")}
+                                KeyboardButtonProps={{
+                                    'aria-label': 'change date',
+                                }}
+                                shouldDisableDate={disableWeekends}
+                            />
+                        </Grid>
+                    </MuiPickersUtilsProvider>
                     <DialogContentText>Recievers:</DialogContentText>
-                    {/* <Paper component="ul" className={classes.root}>
-                        {msgForm.reciever_ids.map((user, i) => {
+                    <Paper component="ul" className={classes.root}>
+                        {apiUsers.map((user, i) => {
                             return (
                                 <li key={i}>
                                     <Chip
@@ -125,80 +236,47 @@ const NewRequestShifts = ({ apiUsers, isLoading, error, fetchUsers, addRequestSh
                                 </li>
                             );
                         })}
-                    </Paper> */}
-                    {/* <TextField
-                        // className={classes.textField}
-                        autoFocus
-                        margin="dense"
-                        id="textFieldSubject"
-                        label="dateFrom"
-                        name="dateFrom"
-                        fullWidth
-                        multiline
-                        rows={2}
-                        type="date"
-                        // value={msgForm.subject}
-                        onChange={e => updateForm(e.target.name, e.target.value)}
-                        variant="outlined"
-                    /> */}
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <Grid container justifyContent="space-around">
-                        <KeyboardDatePicker
-                            margin="dense"
-                            id="date-picker-dialog"
-                            label="From"
-                            format="dd/MM/yyyy"
-                            value={fromDate}
-                            // name="from"
-                            onChange={handleFromDateChange}
-                            KeyboardButtonProps={{
-                                'aria-label': 'change date',
-                            }}
-                            // size="small"
-                            />
+                    </Paper>
+                    {/* MuiFormGroup-root makeStyles-cb-6 MuiFormGroup-row */}
+                    {checkbox.map((cb, i) => {
+                        return (
+                            // <FormControl className={classes.formControl}>
+                            <FormControl key={i} fullWidth >
+                                <FormLabel component="legend">{cb.date}</FormLabel>
+                                <Paper className={classes.paper}>
+                                    {/* <DialogContentText>lol</DialogContentText> */}
 
-                        <KeyboardDatePicker
-                            // size="small"
-                            margin="dense"
-                            id="date-picker-dialog"
-                            label="To"
-                            format="dd/MM/yyyy"
-                            value={toDate}
-                            // name="to"
-                            onChange={handleToDateChange}
-                            KeyboardButtonProps={{
-                                'aria-label': 'change date',
-                            }}
-                        />
-                        </Grid>
-                    </MuiPickersUtilsProvider>
-                    <TextField
-                        className={classes.textField}
-                        autoFocus
-                        margin="dense"
-                        id="textFieldSubject"
-                        label="Subject"
-                        name="subject"
-                        fullWidth
-                        multiline
-                        rows={2}
-                        // value={msgForm.subject}
-                        onChange={e => updateForm(e.target.name, e.target.value)}
-                        variant="outlined"
-                    />
-                    <TextField
-                        className={classes.textField}
-                        margin="normal"
-                        id="textFieldBody"
-                        label="Body"
-                        name="body"
-                        fullWidth
-                        multiline
-                        rows={6}
-                        // value={msgForm.body}
-                        onChange={e => updateForm(e.target.name, e.target.value)}
-                        variant="outlined"
-                    />
+                                    <FormGroup row className={classes.formControl}>
+                                        <FormControlLabel
+                                            control={<Checkbox checked={cb.morningCheckbox} onChange={handleCheckboxChange} color="primary" />}
+                                            label="Morning"
+                                        />
+                                        <FormControlLabel
+                                            control={
+                                                <Checkbox checked={cb.noonCheckbox} onChange={handleCheckboxChange} color="primary" />}
+                                            label="Noon"
+                                        />
+                                        {cb.eveningCheckbox === "disabled" ?
+                                            <FormControlLabel disabled control={<Checkbox />} label="Evening" />
+                                            :
+                                            <FormControlLabel
+                                                control={
+                                                    <Checkbox checked={cb.eveningCheckbox} onChange={handleCheckboxChange} color="primary" />}
+                                                label="Evening"
+                                            />
+                                        }
+                                    </FormGroup>
+                                </Paper>
+
+                            </FormControl>
+
+                        )
+                    })}
+
+                    {/* <FormGroup>
+                        <FormControlLabel disabled control={<Checkbox name="checkedD" />} label="Disabled" />
+                    </FormGroup> */}
+
                 </DialogContent>
                 <DialogActions className={classes.msgBtn}>
                     <Button className="msgBtn" onClick={handleClose} color="primary" >
